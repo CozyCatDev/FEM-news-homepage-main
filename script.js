2208cc96f855b135509ce3c2ab64dd912f36689b
CozyCat <cozycatdev@gmail.com>
2024-02-27 14:41:17 +0800

    fixed nav menu sliding in from the right initially, fixed nav overlay

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ const navOverlay = document.querySelector(".overlay");
 document.querySelector(".open-nav-btn").addEventListener("click", () => {
     nav.dataset.active = "true";
     navOverlay.dataset.active = "true";
+    navOverlay.style.visibility = "visible";
+    navOverlay.style.opacity = "1";
 });
 document.querySelector(".close-nav-btn").addEventListener("click", () => {
     nav.dataset.active = "false";
     navOverlay.dataset.active = "false";
+    navOverlay.style.opacity = "0";
+    setTimeout(() => {
+        navOverlay.style.visibility = "hidden";
+    },250);
 });
